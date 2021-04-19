9bef908dcc3a0538812a06e981699a5833582cf9
SudeshChinke <77224719+SudeshChinke@users.noreply.github.com>
2021-04-19 19:22:25 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,33 @@
+class Particle {
+    constructor(x, y) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:false
+        }
+        this.r = 10;
+        this.colour = color(random(20,255), random(20,255), random(20,255));
+        this.body = Bodies.circle(x, y, this.r,options);
+        
+        
+        World.add(world, this.body);
+
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        fill(this.colour);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+
+}
+
